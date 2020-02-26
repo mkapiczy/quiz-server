d3204c91de42c380c232e470130fdfa8ab2b0ea9
nh51955-hertz-net <michal.kapiczynski@hertz.com>
2020-02-26 18:41:29 +0000

    Init project with SpringInitializr

---
+++ src/main/java/pl/grapeup/mika/quizserver/QuizServerApplication.java
@@ -0,0 +1,13 @@
+package pl.grapeup.mika.quizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServerApplication.class, args);
+	}
+
+}
